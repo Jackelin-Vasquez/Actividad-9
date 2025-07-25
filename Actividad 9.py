99a509c6f689e33b16b633e3ad95816bb6712786
Jackelin <jackelinvasquezguzman@gmail.com>
2025-07-25 17:35:43 -0600

    Menú principal y funcion opcion uno

--- Actividad 9.py
+++ Actividad 9.py
@@ -3,8 +3,8 @@ def opcion_uno():
     cantidad_peliculas= input(input("Ingrese cantidad de peliculas a ingresar:"))
     for i in range(cantidad_peliculas):
         titulo= input(f"Ingrese nombre de pelicula {i+1}:")
-        año_estreno= int(input(f"Ingrese año de estreño de pelicula {i+1}"))
-        genero=input(f"Ingrese genero de pelicula {i+1}")
+        año_estreno= int(input(f"Ingrese año de estreño de pelicula {i+1}:"))
+        genero=input(f"Ingrese genero de pelicula {i+1}:")
         peliculas.append([titulo,año_estreno,genero])
 while True:
     print("---MENÚ---")
